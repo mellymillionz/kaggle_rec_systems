ac7fc2ce33f0fb0cd7d5b67580234f3ff6a5ce73
mellymillionz <melissamunz@gmail.com>
2020-02-26 17:41:08 -0500

    intialize

---
+++ config_final.py
@@ -0,0 +1,15 @@
+#AWS password information
+password = 'T3mp~2019~!123'
+host = 'flatiron.cvsromhnb9qk.us-east-2.rds.amazonaws.com'
+user = 'munzma'
+
+#Yelp API key
+yelp_api_key = 'd5QDU2Jqa6xT5yatO96Ii_pYyd7G00c0NTh_LCwb0__bfzCiWpvuR-dz2PZI2vrlHfTbiom6WelvaX0DTQMHsAodCHjshoBVPtosOFiR5ZlVrKfv1ljnWYlmL9yxXXYx'
+
+#SQL Alchemy
+alc_key = "mysql://munzma:T3mp~2019~!123@flatiron.cvsromhnb9qk.us-east-2.rds.amazonaws.com"
+
+#US.GOV API key
+gov_key = 'GbZ24exMexyJarvqWy7MaepcKwWDzwhDPoEqnXXf'
+
+
